8f231d08deb442780b187f701d517a353c140f7b
Albert Fazlyev <albert.fz@yandex.ru>
2024-10-17 02:07:37 +0500

    refactor

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,31 +1,17 @@
+from itertools import islice
 from http import HTTPStatus
-from typing import Annotated, Optional
+from typing import Annotated
 from fastapi import FastAPI, HTTPException, Query, Response
-from collections import OrderedDict
-from dataclasses import asdict, replace
-from itertools import groupby, islice
-from types import SimpleNamespace
 from fastapi.responses import JSONResponse
-from httpx import delete
-from lecture_2.hw.shop_api.utils import get_last_key
-from lecture_2.hw.shop_api.models import Cart, Item
+from lecture_2.hw.shop_api.models import Cart, Item, storage
 from lecture_2.hw.shop_api.schemas import ItemCreate, ItemReplace, ItemUpdate
 
 app = FastAPI(title="Shop API")
 
-storage = SimpleNamespace(
-    items=OrderedDict(),
-    carts=OrderedDict(),
-)
-
-
-
 @app.post("/cart")
 async def create_cart():
-    id = get_last_key(storage.carts, -1) + 1
+    id = storage.carts.get_last_id()
     storage.carts[id] = Cart(id = id)
-    
-
     return JSONResponse(
         content={'id': id},
         headers={
@@ -35,31 +21,12 @@ async def create_cart():
     )
 
 @app.get("/cart/{id}")
-async def get_cart(id: int):
+async def get_cart(id: int) -> Cart:
     cart = storage.carts[id]
-    key = lambda x: x.id
+    if not cart:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Cart not found")
+    return cart
 
-    grouped_items = groupby(
-        sorted(cart.items, key=key),
-        key=key
-    )
-
-    grouped_items = (
-        list(group) for _, group in grouped_items
-    ) 
-
-    return asdict(cart) | {
-        'items': [
-            {
-                'id': group[0].id,
-                'name': group[0].name,
-                'quantity': len(group),
-                'available': group[0].deleted
-            }
-            for group in grouped_items
-        ],
-        'price': cart.calculate_price()
-    }
 
 @app.post("/cart/{cart_id}/add/{item_id}")
 async def add_item_to_cart(cart_id: int, item_id: int):
@@ -71,29 +38,25 @@ async def add_item_to_cart(cart_id: int, item_id: int):
     if item is None:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Item not found")
   
-    cart.items.append(item)
+    cart.add_item(item)
 
     return Response(status_code=204)
 
 
-@app.post("/item")
-async def create_item(data: ItemCreate):
-    id = get_last_key(storage.items, -1) + 1
+@app.post("/item", status_code=HTTPStatus.CREATED)
+async def create_item(data: ItemCreate, response: Response) -> Item:
+    id = storage.items.get_last_id()
     storage.items[id] = Item(
         id=id,
         name=data.name,
         price=data.price
     )
-    return JSONResponse(
-        content=asdict(storage.items[id]),
-        headers={
-            'Location': f'/item/{id}'
-        },
-        status_code=HTTPStatus.CREATED
-    )
+    response.headers['Location'] = f'/item/{id}'
+    return storage.items[id]
+
 
 @app.get("/item/{id}")
-async def get_item(id: int):
+async def get_item(id: int) -> Item:
     item = storage.items.get(id)
     
     if not item or item.deleted:
@@ -108,7 +71,7 @@ async def get_items(
     min_price: Annotated[float | None, Query(ge=0)] = None,
     max_price: Annotated[float | None, Query(ge=0)] = None,
     show_deleted: bool = False
-):
+) -> list[Item]:
     items = (
         item for item in storage.items.values()
         if item.price > (min_price or float('-inf')) and
@@ -120,14 +83,16 @@ async def get_items(
 
 
 @app.put("/item/{id}")
-async def replace_item(id: int, data: ItemReplace):
+async def replace_item(id: int, data: ItemReplace) -> Item:
     item = storage.items.get(id)
-
     if not item:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Item not found")
-  
-    storage.items[id] = replace(item, **data.model_dump())
-    return storage.items[id]
+    
+    item.name = data.name
+    item.price = data.price
+
+    return item 
+
 
 @app.patch("/item/{id}")
 async def update_item(id: int, data: ItemUpdate):
@@ -135,22 +100,16 @@ async def update_item(id: int, data: ItemUpdate):
     if not item:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Item not found")
    
-    print(item)
     if item.deleted:
         return Response(status_code=HTTPStatus.NOT_MODIFIED)
     
-    new_item = replace(
-        item,
-        **{
-            key: value for key, value in data.model_dump().items()
-            if value is not None
-        }
-    )
+    if item.name is not None:
+        item.name = data.name
 
-    storage.items[id] = new_item
-
-    return new_item
-
+    if item.price is not None:
+        item.price = data.price
+    
+    return item
 
 @app.delete("/item/{id}")
 async def delete_item(id: int):
--- lecture_2/hw/shop_api/models.py
+++ lecture_2/hw/shop_api/models.py
@@ -1,21 +1,58 @@
-from dataclasses import dataclass, field
+from collections import Counter, OrderedDict
+from pydantic import BaseModel, PrivateAttr, computed_field
+from types import SimpleNamespace
 
-@dataclass
-class Item:
+
+class SimpleStorage(OrderedDict):
+    def get_last_id(self):
+        last_id = next(reversed(self), -1)
+        return last_id + 1
+
+storage = SimpleNamespace(
+    items=SimpleStorage(),
+    carts=SimpleStorage(),
+)
+
+class Item(BaseModel):
     id: int
     name: str
     price: float
     deleted: bool = False
 
-@dataclass
-class Cart:
+class CartPosition(BaseModel):
     id: int
-    items: list[Item] = field(default_factory=list)
+    name: str
+    quantity: int
+    available: bool
 
-    def calculate_price(self) -> float:
+class ItemWrap(BaseModel):
+    item: Item
+
+    def __hash__(self):
+        return self.item.id
+
+class Cart(BaseModel):
+    id: int
+    _counts: Counter = PrivateAttr(default_factory=Counter)
+    
+    def add_item(self, item: Item): 
+        self._counts[ItemWrap(item=item)] += 1
+
+    @computed_field
+    @property
+    def items(self) -> list[CartPosition]:
+        return [
+            CartPosition(
+                id=wrap.item.id, name=wrap.item.name, quantity=quantity, available=wrap.item.deleted
+            )
+            for wrap, quantity in self._counts.items()
+        ]
+
+    @computed_field
+    @property
+    def price(self) -> float:
         return sum(
-            item.price for item in self.items
-            if not item.deleted
+            wrap.item.price * quantity
+            for wrap, quantity in self._counts.items()
         )
 
-        
